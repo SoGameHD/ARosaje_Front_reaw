4eb532238a5d92cd2203069e036bd5b2b1690f14
Anymok <arthur.heude@laposte.net>
2023-03-29 00:13:50 +0200

    add jwtToken

--- src/app/views/services/Api.js
+++ src/app/views/services/Api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
+localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcnRodXIuaGV1ZGVAb3V0bG9vay5mciIsImlhdCI6MTY4MDA0MTQwNywiZXhwIjoxNjgwMDQyODQ3fQ.PT_LMJY-XMptk6wdfsSmY5yRvehNeBEI2AOJglYegMs');
+
+const token = localStorage.getItem('token');
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
+    Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
