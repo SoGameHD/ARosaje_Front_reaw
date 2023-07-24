d2e27a4ba0bec1e80ba0eeedbebc18660abb252b
Robin667 <robin.turpin8@gmail.com>
2023-07-24 11:18:54 +0200

    reordercode

--- src/app/views/services/auth.service.js
+++ src/app/views/services/auth.service.js
@@ -1,36 +1,34 @@
-import api from './axiosConfig';
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
-
+import api from './axiosConfig'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080/',
   headers: {
     'Content-Type': 'application/json',
-    Accept: 'application/json',
+    Accept: 'application/json'
   },
-  timeout: 10000,
-});
-
+  timeout: 10000
+})
 
 const authenticate = async (jsonUser) => {
-    const isValid = checkToken();
-    if(!isValid) {
-      try {
-        const response = await instance.post(`/auth/authenticate`, jsonUser)
-        localStorage.setItem("token", response.data.token)
-        return response.data
-      } catch (error) {
-        console.error(error)
-      }
-    } else {
-      return true
+  const isValid = checkToken()
+  if (!isValid) {
+    try {
+      const response = await instance.post(`/auth/authenticate`, jsonUser)
+      localStorage.setItem('token', response.data.token)
+      return response.data
+    } catch (error) {
+      console.error(error)
     }
+  } else {
+    return true
+  }
 }
 
 const register = async (jsonUser) => {
   try {
-    localStorage.clear();
+    localStorage.clear()
     const response = await instance.post(`/auth/register`, jsonUser)
     return response.data
   } catch (error) {
@@ -39,16 +37,16 @@ const register = async (jsonUser) => {
 }
 
 const logout = async () => {
-  localStorage.clear();
-  window.history.pushState({}, '', '/connexion');
-  window.location.reload();
+  localStorage.clear()
+  window.history.pushState({}, '', '/connexion')
+  window.location.reload()
 }
 
 const getCurrentUser = async () => {
   try {
-    const email = getCurrentUserEmail();
-    if(email != false) {
-      const response = await api.get(`/getUser?email=`+ email)
+    const email = getCurrentUserEmail()
+    if (email != false) {
+      const response = await api.get(`/getUser?email=` + email)
       return response.data
     } else {
       return false
@@ -59,37 +57,29 @@ const getCurrentUser = async () => {
 }
 
 function getCurrentUserEmail() {
-let token = localStorage.getItem("token");
-if(token) {
-  let decodedToken = jwtDecode(token);
-  return decodedToken.sub;
-} else {
-  return false;
-}
-
+  let token = localStorage.getItem('token')
+  if (token) {
+    let decodedToken = jwtDecode(token)
+    return decodedToken.sub
+  } else {
+    return false
+  }
 }
 
 function checkToken() {
-  let token = localStorage.getItem("token");
-  if(token) {
-    let decodedToken = jwtDecode(token);
-    let currentDate = new Date();
+  let token = localStorage.getItem('token')
+  if (token) {
+    let decodedToken = jwtDecode(token)
+    let currentDate = new Date()
     if (decodedToken.exp * 1000 < currentDate.getTime()) {
-      localStorage.clear();
-      return false;
+      localStorage.clear()
+      return false
     } else {
-      return true;
+      return true
     }
   } else {
-    return false;
+    return false
   }
 }
 
-export {
-    authenticate,
-    logout,
-    register,
-    getCurrentUser,
-    checkToken
-  }
-  
+export { authenticate, logout, register, getCurrentUser, checkToken }
